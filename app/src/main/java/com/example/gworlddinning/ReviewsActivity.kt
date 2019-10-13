a7dcc0804fd9e7ba65ce337f716cf252ed912a0c
MengyuanWei <rebec0916@gwu.edu>
2019-10-13 19:26:53 -0400

    Add ReviewsAdapter kotlin file

--- app/src/main/java/com/example/gworlddinning/ReviewsActivity.kt
+++ app/src/main/java/com/example/gworlddinning/ReviewsActivity.kt
@@ -3,6 +3,7 @@ package com.example.gworlddinning
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import androidx.recyclerview.widget.RecyclerView
 
@@ -21,6 +22,18 @@ class ReviewsActivity : AppCompatActivity() {
 //        title = "Reviews near $location"
 
         recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Set the adapter.
+        // recyclerView.adapter =
+    }
+
+    fun getFakeReviews(): List<Review>{
+        return listOf(
+                Review(name="Nia G.", content = "Great takeout food. Great prices and hella quick! This is my third order with May Island and the food was pretty good.", ratingBar = 3.5),
+                Review(name = "Adianna P.", content = "My 2nd time ordering from May Island is great as always!", ratingBar = 4.0),
+                Review(name = "Jason J.", content = "I want to say that my family really enjoy May Island.", ratingBar = 5.0)
+        )
     }
 
 }
---
+++ app/src/main/java/com/example/gworlddinning/ReviewsAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.gworlddinning
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+// abstract class
+class ReviewsAdapter(val reviews: List<Review>): RecyclerView.Adapter<ReviewsAdapter.ReviewsViewHolder>(){
+
+    // RecyclerView needs a new XML layout for a new row.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewsViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    // How many rows (data items) the RecyclerView should render in total.
+    override fun getItemCount(): Int {
+        // TODO("not implemented")
+        // To change body of created functions use File | Settings | File Templates.
+        return reviews.size
+    }
+
+    // RecyclerView needs data to fill a new row,
+    override fun onBindViewHolder(holder: ReviewsViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ReviewsViewHolder(view: View): RecyclerView.ViewHolder(view){
+        // Constraint layout.
+        val name: TextView = view.findViewById(R.id.name)
+        val content: TextView = view.findViewById(R.id.review_content)
+        val ratingBar: RatingBar = view.findViewById(R.id.ratingBar)
+
+    }
+
+
+}
